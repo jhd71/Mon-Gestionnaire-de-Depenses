c510b0c13b31bb5deb4237f3f7926e7a5621a508
JHD <herve71@free.fr>
2025-08-07 03:34:33 +0200

    pdf

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 // sw.js - Service Worker amélioré pour PWA complète
 
-const CACHE_NAME = 'gestionnaire-depenses-v2.1';
+const CACHE_NAME = 'gestionnaire-depenses-v2.2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
   '/manifest.json',
+  '/js/pdf-export.js',
   '/favicon.ico',
   '/images/icon-192.png',
   '/images/icon-512.png',
