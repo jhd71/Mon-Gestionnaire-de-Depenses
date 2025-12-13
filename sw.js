7fa28e6f44ef1fc1a0705cd72741641c8681c5f7
jhd71 <44101959+jhd71@users.noreply.github.com>
2025-12-13 04:45:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker amélioré pour PWA complète
-const CACHE_NAME = 'gestionnaire-depenses-v5';
+const CACHE_NAME = 'gestionnaire-depenses-v10';
 const urlsToCache = [
   '/',
   '/index.html',
